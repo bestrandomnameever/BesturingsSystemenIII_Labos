58e8eb54bfe1b0662d1f609222527f45088d8cbf
Anthony <bestrandomnameever@gmail.com>
2018-10-05 10:32:00 +0200

    Reeks 2 Oef 3

--- Reeks 2/3.pl
+++ Reeks 2/3.pl
@@ -5,8 +5,11 @@ $fso = Win32::OLE->new("Scripting.FileSystemObject");
 $excelApp = Win32::OLE->GetActiveObject('Excel.Application') || Win32::OLE->new('Excel.Application', 'Quit');
 
 if ($fso->FileExists($ARGV[0]) && ($file = $fso->GetFile($ARGV[0]))->{Type} =~ /Excel/) {
-	printf "Marche\n";
-	
+	$book = $excelApp->Workbooks->Open($file->Path);
+	foreach $sheet (in $book->Worksheets) {
+		$range = $sheet->UsedRange;
+		printf "Sheet %s heeft %s rijen en %s kolommen\n", $sheet->Name, $range->Rows->Count, $range->Columns->Count;
+	}
 }else {
 	printf "Bestand %s is een onbestaand of ongeldig Excel document\n", $ARGV[0];
 }
